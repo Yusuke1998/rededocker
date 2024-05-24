a25230dd61906cadd2c1ad4a843c18a3d12c436a
yusuke1998 <jhonnyjose1998@gmail.com>
2024-05-23 20:36:15 -0400

    [ADD] Refactor y correcciones
    - Ajustes para que sea compatible con linux.

--- run.sh
+++ run.sh
@@ -18,7 +18,8 @@ source .env
 source "${LARAVEL_PATH}/.env"
 set +a
 
-#LARAVEL_PATH=$(realpath $LARAVEL_PATH)
+LARAVEL_PATH=$(realpath $LARAVEL_PATH)
+NUXT_PATH=$(realpath $NUXT_PATH)
 
 if [ ! -d "$LARAVEL_PATH" ]; then
     echo "Directorio del proyecto Laravel no encontrado!"
@@ -30,19 +31,27 @@ if [ ! -f "$LARAVEL_PATH/vendor/bin/sail" ]; then
     exit 1
 fi
 
+cp ./front/Dockerfile $NUXT_PATH
+
 if [ $# -eq 0 ]; then
     echo "Por favor, proporciona un argumento: up o down"
     exit 1
 fi
 
+cat ${LARAVEL_PATH}/.env ${SELF_PATH}/.env > merged.env
+
 if [ "$1" == "artisan" ]; then
     shift 1
     CMD="${LARAVEL_PATH}/vendor/bin/sail artisan $@"
 else
-    CMD="${LARAVEL_PATH}/vendor/bin/sail --file ${SELF_PATH}/docker-compose.yml --env-file ${SELF_PATH}/.env --env-file ${LARAVEL_PATH}/.env $@"
+    CMD="${LARAVEL_PATH}/vendor/bin/sail --file ${SELF_PATH}/docker-compose.yml --env-file merged.env $@"
 fi
 
-exec $CMD
+$CMD
+
+if [ -f "$NUXT_PATH/Dockerfile" ]; then
+    rm $NUXT_PATH/Dockerfile
+fi
 
 echo "Fin de script."
 exit 0
